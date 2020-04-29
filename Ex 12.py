a3bda06a106ead5cc2ddc5aabe96cf30082f07cc
FatemehRabie <63732123+FatemehRabie@users.noreply.github.com>
2020-04-29 17:51:54 +0430

    Add files via upload

---
+++ Ex 12.py
@@ -0,0 +1,22 @@
+#%%
+print('___________    Ex 12    _______________ ')
+
+import tkinter as tk
+win=tk.Tk()
+lb1=tk.Label(win,text='User')
+en1=tk.Entry()
+lb2=tk.Label(win,text='pass')
+en2=tk.Entry()
+
+bt1=tk.Button(win)
+lb1.pack()
+lb2.pack()
+en1.pack()
+en2.pack()
+bt1.pack()
+win.mainloop()
+
+
+
+
+# %%
