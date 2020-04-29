f0b014ef744bf531541e27ea7d1d5106e9bcedc3
FatemehRabie <63732123+FatemehRabie@users.noreply.github.com>
2020-04-29 18:10:07 +0430

    Add files via upload

--- Ex 12.py
+++ Ex 12.py
@@ -7,8 +7,8 @@ lb1=tk.Label(win,text='User')
 en1=tk.Entry()
 lb2=tk.Label(win,text='pass')
 en2=tk.Entry()
+btn1 = tk.Button(win, text = 'log in',command = func)
 
-bt1=tk.Button(win)
 lb1.pack()
 lb2.pack()
 en1.pack()
@@ -18,5 +18,27 @@ win.mainloop()
 
 
 
+# %%
+import tkinter as tk
+win = tk.Tk()
+password = '12345'
+user = 'admin'
+def func():
+    if en1.get() == user and en2.get() == password:
+        btn1.config(background = 'green')
+    else:
+        btn1.config(background = 'red')
+        
+lb1 = tk.Label(win, text = 'user name')
+en1 = tk.Entry(win)
+lb2 = tk.Label(win, text = 'password')
+en2 = tk.Entry(win)
+btn1 = tk.Button(win, text = 'log in',command = func)
+lb1.pack()
+en1.pack()
+lb2.pack()
+en2.pack()
+btn1.pack()
+win.mainloop()
 
 # %%
